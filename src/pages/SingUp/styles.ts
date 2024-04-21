adff33edac45c15cdaf53db26227df792109ba9e
José Lucas <gomessze03@gmail.com>
2024-04-21 09:48:50 -0300

    Add user creation screen and Readme. Closes #3

---
+++ src/pages/SingUp/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import styles from '../../styles';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${styles.colors.background};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.View`
+    width: 80%;
+`;
