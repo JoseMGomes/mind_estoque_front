b505bef6ca74099efcc2de3700125eebcb834be7
José Lucas <gomessze03@gmail.com>
2024-04-21 09:42:24 -0300

    Revert "Add user creation screen"
    This reverts commit 9957bd6dcfeacd06dc723137194c1ff4ee18771c.

--- App.tsx
+++ App.tsx
@@ -2,11 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/pages/Login';
 import Home from './src/pages/Home';
-import SingUp from './src/pages/SingUp';
 
 export default function App() {
   return (
-   <SingUp/>
+   <Home/>
   );
 }
 
